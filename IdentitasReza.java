39eead16dd186c4d9d09798d072302aa2fe094e4
RezhaArifin <90241950+RezhaArifin@users.noreply.github.com>
2021-09-14 15:17:44 +0700

    Add files via upload

---
+++ IdentitasReza.java
@@ -0,0 +1,16 @@
+public class IdentitasReza {
+    public static void main(String[] args) {
+        String namaLengkap = "Mochamad Reza Nur Arifin";
+        String namaPanggilan = "Reza";
+        String NIM = "21537144032";
+        int angkatan = 2021;
+        String programStudi = "S1 Teknologi Informasi";
+        String fakultas = "Fakultas Teknik";
+        double IPKharapan = 3.8;
+        System.out.println("Perkenalkan nama lengkap saya " + namaLengkap + " " + "biasa dipanggil " + namaPanggilan);
+        System.out.println("NIM saya " + NIM);
+        System.out.println("Saya mahasiswa UNY angkatan " + angkatan);
+        System.out.println("Fakultas saya di bidang " + fakultas + " " + "jurusan " + programStudi);
+        System.out.println("Sebagai mahasiswa UNY saya berharap mendapat IPK " + IPKharapan);
+    }    
+}
---
+++ identitasreza.py
@@ -0,0 +1,12 @@
+namaLengkap = "Mochamad Reza Nur Arifin"
+namaPanggilan = "Reza"
+NIM = 21537144032
+angkatan = 2021
+programStudi = "S1 Teknologi Informasi"
+fakultas = "Fakultas Teknik"
+IPKharapan = 3.8
+print("Perkenalkan nama lengkap saya " + namaLengkap + " " + "biasa dipanggil " + namaPanggilan)
+print("NIM saya " + str(NIM))
+print("Saya mahasiswa UNY angkatan " + str(angkatan))
+print("Fakultas saya di bidang " + fakultas + " " + "jurusan " + programStudi)
+print("Sebagai mahasiswa UNY saya berharap mendapat IPK " + str(IPKharapan))
